3ecd683bc2cc9ae1d87655d5252a64fe9623861a
Christian Anderson <christian@techonomics.org>
2021-08-10 13:24:45 -0700

    Initial commit

---
+++ client/script.js
@@ -0,0 +1,160 @@
+// A reference to Stripe.js
+var stripe;
+
+var orderData = {
+  items: [{ id: "photo-subscription" }],
+  currency: "usd"
+};
+
+fetch("/stripe-key")
+  .then(function(result) {
+    return result.json();
+  })
+  .then(function(data) {
+    return setupElements(data);
+  })
+  .then(function({ stripe, card, clientSecret }) {
+    document.querySelector("#submit").addEventListener("click", function(evt) {
+      evt.preventDefault();
+      document.querySelector("#submit").disabled = true;
+      pay(stripe, card, clientSecret);
+      document.querySelector("#submit").disabled = false;
+    });
+  });
+
+var setupElements = function(data) {
+  stripe = Stripe(data.publicKey);
+  /* ------- Set up Stripe Elements to use in checkout form ------- */
+  var elements = stripe.elements();
+  var style = {
+    base: {
+      color: "#32325d",
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#aab7c4"
+      }
+    },
+    invalid: {
+      color: "#fa755a",
+      iconColor: "#fa755a"
+    }
+  };
+
+  var card = elements.create("card", { style: style });
+  card.mount("#card-element");
+
+  return {
+    stripe,
+    card,
+    clientSecret: data.clientSecret
+  };
+};
+
+var handleAction = function(clientSecret) {
+  stripe.handleCardAction(clientSecret).then(function(data) {
+    if (data.error) {
+      showError("Your card was not authenticated, please try again");
+    } else if (data.paymentIntent.status === "requires_confirmation") {
+      fetch("/pay", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          paymentIntentId: data.paymentIntent.id
+        })
+      })
+        .then(function(result) {
+          return result.json();
+        })
+        .then(function(json) {
+          if (json.error) {
+            showError(json.error);
+          } else {
+            orderComplete(clientSecret);
+          }
+        });
+    }
+  });
+};
+
+/*
+ * Calls stripe.handleCardPayment which creates a pop-up modal to
+ * prompt the user to enter  extra authentication details without leaving your page
+ */
+var pay = function(stripe, card) {
+  var cardholderName = document.querySelector("#name").value;
+  var data = {
+    billing_details: {}
+  };
+
+  if (cardholderName) {
+    data["billing_details"]["name"] = cardholderName;
+  }
+
+  // Initiate the payment. handleCardPayment will display a modal
+  stripe
+    .createPaymentMethod("card", card, data)
+    .then(function(result) {
+      if (result.error) {
+        showError(result.error.message);
+      } else {
+        orderData.paymentMethodId = result.paymentMethod.id;
+
+        return fetch("/pay", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(orderData)
+        });
+      }
+    })
+    .then(function(result) {
+      return result.json();
+    })
+    .then(function(paymentData) {
+      if (paymentData.requiresAction) {
+        // Request authentication
+        handleAction(paymentData.clientSecret);
+      } else if (paymentData.error) {
+        showError(paymentData.error);
+      } else {
+        orderComplete(paymentData.clientSecret);
+      }
+    });
+};
+
+/* ------- Post-payment helpers ------- */
+
+/* Shows a success / error message when the payment is complete */
+var orderComplete = function(clientSecret) {
+  stripe.retrievePaymentIntent(clientSecret).then(function(result) {
+    var paymentIntent = result.paymentIntent;
+    var paymentIntentJson = JSON.stringify(paymentIntent, null, 2);
+    document.querySelectorAll(".payment-view").forEach(function(view) {
+      view.classList.add("hidden");
+    });
+    document.querySelectorAll(".completed-view").forEach(function(view) {
+      view.classList.remove("hidden");
+    });
+    document.querySelector(".order-status").textContent =
+      paymentIntent.status === "succeeded" ? "succeeded" : "failed";
+    document.querySelector("pre").textContent = paymentIntentJson;
+  });
+};
+
+var showError = function(errorMsgText) {
+  var errorMsg = document.querySelector(".sr-field-error");
+  errorMsg.textContent = errorMsgText;
+  setTimeout(function() {
+    errorMsg.textContent = "";
+  }, 4000);
+};
+
+// Hide banner if window is too small or in iframe
+if (window.outerHeight < 600 || window.location !== window.parent.location) {
+  document.querySelector(".banner").classList.add("hidden");
+}
---
+++ server/README.md
@@ -0,0 +1,5 @@
+# Running the server
+
+CodeSandbox only supports Node runtime, please see the GitHub repository for examples in other languages.
+
+https://github.com/stripe-samples/placing-a-hold
